cf0c2cb7610ee8080d93176e6dcca0efd7c2e53d
ssawinsp <iriejamaica@outlook.com>
2018-05-31 08:39:56 +0300

    Objects tasks updated

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -24,7 +24,11 @@
  *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
-    throw new Error('Not implemented');
+    var rectangle = {};
+    rectangle.width = width;
+    rectangle.height = height;
+    rectangle.getArea = function () { return this.height * this.width; }
+    return rectangle;
 }
 
 
@@ -39,7 +43,7 @@ function Rectangle(width, height) {
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
 function getJSON(obj) {
-    throw new Error('Not implemented');
+    return JSON.stringify(obj);
 }
 
 
